e9c8c9822737a0627c8a7731f0a128818729e3e3
Son-Chaeyi <chaeyi0318@gmail.com>
2023-02-11 22:03:01 +0900

    myselectshop

--- src/main/java/com/sparta/myselectshop/controller/ShopController.java
+++ src/main/java/com/sparta/myselectshop/controller/ShopController.java
@@ -1,11 +1,16 @@
 package com.sparta.myselectshop.controller;
 
+import com.sparta.myselectshop.entity.User;
+import com.sparta.myselectshop.jwt.JwtUtil;
+import com.sparta.myselectshop.repository.UserRepository;
 import com.sparta.myselectshop.service.FolderService;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,6 +21,8 @@ import javax.servlet.http.HttpServletRequest;
 public class ShopController {
 
     private final FolderService folderService;
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
 
     @GetMapping("/shop")
     public ModelAndView shop() {
@@ -28,7 +35,34 @@ public class ShopController {
 
         model.addAttribute("folders", folderService.getFolders(request));
 
-        return "/index :: #fragment";
+        return "index :: #fragment";
+    }
+
+    // 로그인 한 유저가 메인페이지를 요청할 때 유저의 이름 반환
+    @GetMapping("/user-info")
+    @ResponseBody
+    public String getUserName(HttpServletRequest request) {
+        String token = jwtUtil.resolveToken(request);
+        Claims claims;
+
+        if (token != null) {
+            // Token 검증
+            if (jwtUtil.validateToken(token)) {
+                // 토큰에서 사용자 정보 가져오기
+                claims = jwtUtil.getUserInfoFromToken(token);
+            } else {
+                throw new IllegalArgumentException("Token Error");
+            }
+
+            // 토큰에서 가져온 사용자 정보를 사용하여 DB 조회
+            User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
+                    () -> new IllegalArgumentException("사용자가 존재하지 않습니다.")
+            );
+
+            return user.getUsername();
+        }else {
+            return "fail";
+        }
     }
 
 }
